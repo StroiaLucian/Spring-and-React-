565d0f6e6bf267a0bc221ecc8201c3073120ddc3
Stroia Lucian Dorin <58705391+StroiaLucian@users.noreply.github.com>
2021-01-05 14:08:45 +0200

    Add files via upload

---
+++ GreenLand-Frontend/build/precache-manifest.30457a54c9846daf411030b74204faae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "48bd743a205df11f397e",
+    "url": "/static/css/main.dbfd92fd.chunk.css"
+  },
+  {
+    "revision": "48bd743a205df11f397e",
+    "url": "/static/js/main.48bd743a.chunk.js"
+  },
+  {
+    "revision": "41ea6ec0f64140105eec",
+    "url": "/static/css/1.b5e426c1.chunk.css"
+  },
+  {
+    "revision": "41ea6ec0f64140105eec",
+    "url": "/static/js/1.41ea6ec0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5706134dde5164d7195bcc13363384db",
+    "url": "/static/media/future-medicine.5706134d.jpg"
+  },
+  {
+    "revision": "63ee281c5d7abbd1089476db45f4f4fe",
+    "url": "/static/media/icon.63ee281c.png"
+  },
+  {
+    "revision": "a64e592fe6611d7dfb1fdced7514c8e8",
+    "url": "/index.html"
+  }
+];
